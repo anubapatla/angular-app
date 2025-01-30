fb2944ed16d18fe0ff92a624efa58adff6fbed8f
SriChatala <sri.chatala@finning.com>
2025-01-29 23:29:15 -0500

    new styles added in homec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import{MatCardModule,} from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './common/about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatCardModule,HomeComponent],
+  imports: [RouterOutlet,MatCardModule,HomeComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/common/about/about.component.ts
+++ src/app/common/about/about.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from "../../home/home.component";
+import { AppComponent } from "../../app.component";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [HomeComponent, AppComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
